5e18221d000ff0b36d3e2d3e0e3371152d3d4a0f
Amy24153 <24153@burnside.school.nz>
2026-05-22 08:56:56 +1200

    maincode added and a few commenrts

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+import sqlite3
+
+#CONTANTS AND VARIABLES
+DATABASE = 'books.db'
+
+#FUNCTIONS
+
+
+#MAIN CODE
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = 'SELECT * FROM books'
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
