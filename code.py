9cdddf18056582d022e7b0d405037a29f3d49468
Amy24153 <24153@burnside.school.nz>
2026-05-28 14:04:38 +1200

    ADDED A LOT OF COMMENTS SO I UNDERSTAND MY CODE

--- code.py
+++ code.py
@@ -6,13 +6,13 @@ import sqlite3
 DATABASE = 'books.db'
 
 #FUNCTIONS
-def print_all_books():
+def print_all_books(): #CREATE A FUNCTION TO PRINT ALL BOOKS IN THE DATABASE
     '''print all the books nicely'''
     db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
+    cursor = db.cursor() #ACTS AS A BRIDGE BETWEEN YOUR PYTHON CODE AND THE DATABASE
     sql = 'SELECT * FROM book;'
-    cursor.execute(sql)
-    results = cursor.fetchall()
+    cursor.execute(sql) #SEND AND RUN SQL COMMANDS AGAIN A DATABASE 
+    results = cursor.fetchall() #FETCH ALL THE RESULTS OF THE QUERY AND STORE THEM IN A VARIABLE CALLED RESULTS - THIS WILL BE A LIST OF TUPLES, WHERE EACH TUPLE REPRESENTS A ROW IN THE BOOK TABLE
 
 #PRINTING THE BOOKS IN A NICE FORMAT
     print("Book ID  | Title                                         | Author                      | Issue Date | Return Date | Status    | Borrower")
@@ -21,13 +21,13 @@ def print_all_books():
          print(f"{row[0]:<8} | {row[1]:<45} | {row[2]:<27} | {row[3]:<10} | {row[4]:<11} | {row[5]:<9} | {row[6]}")
 #PRINTING PERSON TABLE
     print("\nUser ID | First Name          | Last Name            |City        ")
-    print("-" * 50)
+    print("-" * 50) #PRINT A SEPARATOR LINE
     sql = 'SELECT * FROM person;'
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
         print(f"{row[0]:<6} | {row[1]:<20} | {row[2]:<20} | {row[3]:<12}")
-    db.close()
+    db.close() #CLOSE THE DATABASE CONNECTION TO FREE UP RESOURCES AND PREVENT POTENTIAL ISSUES WITH TOO MANY OPEN CONNECTIONS
 #MAIN CODE
 def add_person(): 
     '''Add a new person/borrower record into the person table'''
@@ -41,3 +41,7 @@ def add_person():
     last_name = input("Enter last name: ")
     city = input("Enter city: ")
 
+    #INSERT THE NEW PERSON INTO THE DATABASE USING A PARAMETERIZED QUERY TO PREVENT SQL INJECTION - A PARAMETERIZED QUERY IS A WAY TO WRITE DATABASE QUERIES THAT USES 'PLACEHOLDERS' INSTEAD OF ACTUAL DATA
+    #'?' IS A PLACEHOLDER THAT WILL BE REPLACED WITH THE ACTUAL DATA WHEN THE QUERY IS EXECUTED
+    sql = 'INSERT INTO person VALUES (?, ?, ?, ?);'
+    cursor.execute(sql, (user_id, first_name, last_name, city))
